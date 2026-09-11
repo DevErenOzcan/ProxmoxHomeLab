6c957d23c2448e6b4f5c785411a541d8d5bb2b2c
eren.ozcan <dev.eren.ozcan@proton.me>
2026-09-11 19:44:39 +0300

    feat: consolidate command references into a new commands.md file

--- step2_vm_provisioning.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#
-# Adım 2: Sanal Makine (VM) Kurulumları
-#
-# Bu komut Terraform kullanarak sanal makineleri ve ağ altyapısını kurar.
-# Not: İlk kullanımda "terraform init" yapmanız gerekebilir.
-
-cd 02_vm_provisioning/environments/local && terraform apply -var-file="secret.tfvars"
--- step3_vm_config.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# Adım 3: Sanal Makine Konfigürasyonları
-#
-# Bu komut oluşturulan sanal makinelerin iç ayarlarını ve paket kurulumlarını yapar.
-
-cd 03_vm_config && ansible-playbook site.yml
